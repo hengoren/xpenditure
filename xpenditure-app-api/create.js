fd8e464cf2acd5db312fbae492056d481a983203
hengoren <harrison.engoren@gmail.com>
2019-09-07 13:07:31 -0400

    initial commit. Add a create transaction API

---
+++ xpenditure-app-api/create.js
@@ -0,0 +1,27 @@
+import uuid from "uuid";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+export async function main(event, context) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: "transactions",
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      transactionId: uuid.v1(),
+      merchant: data.merchant,
+      price: data.price,
+      category: data.category,
+      date: data.date,
+      paymentType: data.paymentType,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
