79ae201d419a07e18d86f43069fc56c38587cf91
hengoren <harrison.engoren@gmail.com>
2019-09-08 10:18:25 -0400

    building a basic react app

---
+++ xpenditure-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "xpenditure-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://9qyqz2k14m.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_35DzA1V7P",
+    APP_CLIENT_ID: "3tl53gs8nk2gvl0s6gkb8r3obi",
+    IDENTITY_POOL_ID: "us-east-1:687042eb-93cb-49a4-9d79-4ccbe17135c0"
+  }
+};
---
+++ xpenditure-app-client/src/containers/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div className="lander">
+          <h1>xpenditure</h1>
+          <p>An personal finance app for tracking spending</p>
+        </div>
+      </div>
+    );
+  }
+}
