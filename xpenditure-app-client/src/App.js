413a3fdf58d6e5816adea9e336f940136f7aae4c
hengoren <harrison.engoren@gmail.com>
2019-09-09 22:55:39 -0400

    finalizing login/signup and authentication flow

--- xpenditure-app-client/src/App.js
+++ xpenditure-app-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Auth } from "aws-amplify";
 import Routes from "./Routes";
 import "./App.css";
 
@@ -11,7 +12,8 @@ class App extends Component {
     super(props);
   
     this.state = {
-      isAuthenticated: false
+      isAuthenticated: false,
+      isAuthenticating: true
     };
   }
   
@@ -19,9 +21,25 @@ class App extends Component {
     this.setState({ isAuthenticated: authenticated });
   }
 
-  handleLogout = event => {
+  handleLogout = async event => {
+    await Auth.signOut();
     this.userHasAuthenticated(false);
+    this.props.history.push('./login')
   }  
+
+  async componentDidMount() {
+    try {
+      await Auth.currentSession();
+      this.userHasAuthenticated(true)
+    }
+    catch (e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    this.setState({ isAuthenticating: false });
+  }
   
   render() {
 
@@ -31,6 +49,7 @@ class App extends Component {
     };
 
     return (
+      !this.state.isAuthenticating &&
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
@@ -40,7 +59,7 @@ class App extends Component {
             <Navbar.Toggle />
           </Navbar.Header>
           <Navbar.Collapse>
-            <Nav pullright>
+            <Nav pullright="true">
             {this.state.isAuthenticated
               ? <NavItem onClick={this.handleLogout}>Logout</NavItem>
               : <Fragment>
@@ -62,4 +81,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withRouter(App);
