5828ba4594d6b4964b53bd9841ecf75c3900706f
hengoren <harrison.engoren@gmail.com>
2019-09-10 21:14:21 -0400

    WIP on new transaction form

--- xpenditure-app-client/src/Routes.js
+++ xpenditure-app-client/src/Routes.js
@@ -4,6 +4,7 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
+import NewTransaction from "./containers/NewTransaction";
 import NotFound from "./containers/NotFound";
 
 export default ({ childProps }) =>
@@ -11,6 +12,7 @@ export default ({ childProps }) =>
     <AppliedRoute path="/" exact component={Home} props={childProps} />
     <AppliedRoute path="/login" exact component={Login} props={childProps} />
     <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
+    <AppliedRoute path="/transactions/new" exact component={NewTransaction} props={childProps} />
     { /* Finally, catch all unmatched routes */ }
     <Route component={NotFound} />
   </Switch>;
