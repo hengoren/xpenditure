f04ae5ad5be56dfb32cc617c181bcac1cb0c1547
hengoren <harrison.engoren@gmail.com>
2019-09-11 19:06:41 -0400

    form to post new transaction to dynamo db (note: don't forget to redeploy serverless after making changes)

--- xpenditure-app-client/src/containers/NewTransaction.js
+++ xpenditure-app-client/src/containers/NewTransaction.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { API } from "aws-amplify";
 import LoadingButton from "../components/LoadingButton";
-import config from "../config";
 import "./NewTransaction.css";
 
 export default class NewTransaction extends Component {
@@ -12,12 +12,22 @@ export default class NewTransaction extends Component {
 
     this.state = {
       isLoading: null,
-      content: ""
+      description: "", 
+      merchant: "",
+      price: "", 
+      category: "",
+      datePurchased: "",
+      paymentType: "" 
     };
   }
 
   validateForm() {
-    return this.state.content.length > 0;
+    return this.state.description.length > 0 &&
+            this.state.merchant.length > 0 &&
+            this.state.price.length > 0 &&
+            this.state.category.length > 0 &&
+            this.state.datePurchased.length > 0 &&
+            this.state.paymentType.length > 0;
   }
 
   handleChange = event => {
@@ -26,31 +36,96 @@ export default class NewTransaction extends Component {
     });
   }
 
-  handleFileChange = event => {
-    this.file = event.target.files[0];
-  }
+//   handleFileChange = event => {
+//     this.file = event.target.files[0];
+//   }
 
   handleSubmit = async event => {
     event.preventDefault();
 
-    // if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
-    //   alert(`Please pick a file smaller than ${config.MAX_ATTACHMENT_SIZE/1000000} MB.`);
-    //   return;
-    // }
-
     this.setState({ isLoading: true });
-  }
+
+    try {
+        await this.createTransaction({
+            description: this.state.description,
+            merchant: this.state.merchant,
+            price: this.state.price,
+            category: this.state.category,
+            datePurchased: this.state.datePurchased,
+            paymentType: this.state.paymentType
+        });
+
+        this.props.history.push('/');
+    } catch (e) {
+        alert(e);
+        this.setState({ isLoading: false });
+    }
+}
+createTransaction(transaction) {
+    return API.post('transactions', '/transactions', {
+        body: transaction
+    });
+}
 
   render() {
     return (
       <div className="NewTransaction">
         <form onSubmit={this.handleSubmit}>
-          <FormGroup controlId="content">
-            <FormControl
-              onChange={this.handleChange}
-              value={this.state.content}
-              componentClass="textarea"
-            />
+          <FormGroup controlId="description">
+            <ControlLabel>Description</ControlLabel>
+                <FormControl
+                onChange={this.handleChange}
+                value={this.state.description}
+                componentClass="textarea"
+                />
+          </FormGroup>
+          <FormGroup controlId="merchant">
+            <ControlLabel>Merchant</ControlLabel>
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.merchant}
+                  componentClass="textarea"
+                />
+          </FormGroup>
+          <FormGroup controlId="price">
+              <ControlLabel>Price</ControlLabel>
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.price}
+                  componentClass="textarea"
+                />
+          </FormGroup>
+          <FormGroup controlId="category">
+              <ControlLabel>Category</ControlLabel>
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.category}
+                  componentClass="select"
+                >
+                    <option>Select Category</option>
+                    <option>Utilities</option>
+                    <option>Gas & Transit</option>
+                    <option>Groceries</option>
+                    <option>Eating Out</option>
+                    <option>Entertainment</option>
+                    <option>Miscellaneous</option>
+                </FormControl>
+          </FormGroup>
+          <FormGroup controlId="datePurchased">
+              <ControlLabel>Date Purchased</ControlLabel>
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.datePurchased}
+                  componentClass="textarea"
+                />
+          </FormGroup>
+          <FormGroup controlId="paymentType">
+              <ControlLabel>Payment Type</ControlLabel>
+                <FormControl
+                  onChange={this.handleChange}
+                  value={this.state.paymentType}
+                  componentClass="textarea"
+                />
           </FormGroup>
           {/* <FormGroup controlId="file">
             <ControlLabel>Attachment</ControlLabel>
